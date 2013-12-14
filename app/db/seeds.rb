d40a436b5ea954d79d63383ab3a7ec9cb07a54b0
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2013-12-14 17:33:35 +0200

    Added seeds

--- app/db/seeds.rb
+++ app/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+products = Product.create([
+  {id: 1, link: "http://magazin.ro/produs/1", name: "Nume produs", price: 120.0, currency: "RON", shop_id: 1},
+  {id: 2, link: "http://magazin.ro/produs/1", name: "Nume produs", price: 172.0, currency: "RON", shop_id: 1}
+])
+
+histories = History.create([
+  {id: 1, price: 120.0, date: 1.hour.ago, product_id:1},
+  {id: 2, price: 50.0, date: 2.hours.ago, product_id:1},
+  {id: 3, price: 500.0, date: 5.hours.ago, product_id:2}
+])
+
+shop = Shop.create([
+  {id: 1, name: "Magazin.ro", url: "http://magazin.ro"}
+])
