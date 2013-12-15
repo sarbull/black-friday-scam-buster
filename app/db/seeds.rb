2aaebdd24eeb81d7c56bbac66ba138c27696e903
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2013-12-15 14:30:04 +0200

    Updated seeds.rb

--- app/db/seeds.rb
+++ app/db/seeds.rb
@@ -1,6 +1,4 @@
-User.create!([
-  {email: "sirbunicolaecezar@gmail.com", encrypted_password: "$2a$10$.UUAoVgFXRJTWtLJASilHeAFIlj8kSHRzhOkO789cU7VuaZXpvLpy", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 4, current_sign_in_at: "2013-12-15 08:48:02", last_sign_in_at: "2013-12-15 08:37:32", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1"}
-])
+# encoding: utf-8
 History.create!([
   {price: 120.0, date: "2013-12-15", product_id: 1},
   {price: 50.0, date: "2013-12-15", product_id: 1},
